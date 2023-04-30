be472e143754a78debf5aa22b195a17e32d00b25
“[Mojtaba-Pourkhanlar]” <[mojtabapourkhanlar@gmail.com]>
2023-04-30 23:54:53 +0430

    create new directory

--- src/frontEnd/pages/Cart.js
+++ src/frontEnd/pages/Cart.js
@@ -1,11 +1,12 @@
 import React from "react";
 import CartComponent from "../components/Cart";
+import { DivLayout } from "../helper";
 
 const Cart = () => {
   return (
-    <>
+    <DivLayout>
       <CartComponent />
-    </>
+    </DivLayout>
   );
 };
 
--- src/frontEnd/pages/index.jsx
+++ src/frontEnd/pages/index.jsx
@@ -1,2 +1,3 @@
 export { default as Products } from "./Products";
 export { default as Cart } from "./Cart";
+// export { default as Products } from "./Products";
